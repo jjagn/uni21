71a7d231cc366214e60e3697be4722d5d740cea2
jjagn <72364545+jjagn@users.noreply.github.com>
2021-03-15 20:47:30 +1300

    pd_control v2, now overshooting voltage by factor of roughly 100
    tried to solve for voltage using syms and stuff, turns out i could just differentiate y array from tf function and then use that to calculate error then voltage. plotting requested voltage next to system response. need to iterate for proper gains now.

--- enme203/pd_control.m
+++ enme203/pd_control.m
@@ -1,3 +1,10 @@
+% jackson crawford 
+% enme303 assignment 1
+
+clear
+clc
+close all
+
 M = 1.5; % mass of cart, kg
 km = 0.017; % motor back emf constant, V/rad/s
 kg = 3.7; % gearing ratio
@@ -6,15 +13,43 @@ r = 0.018; % radius of pinion, m
 D = 7; % damping present in physical system, e.g friction
 
 % constants so I don't have to type all that junk out
-B = (km * kg) / (m * R * r);
-C = D / M + (km^2 * kg^2) / (m * R * r^2);
+B = (km * kg) / (M * R * r);
+C = D / M + (km^2 * kg^2) / (M * R * r^2);
 
 % controller gains
-Kd = 1;
-Kp = 100;
+Kd = 8;
+Kp = 1000;
 
 % fraction declaration
 num = [B*Kd B*Kp];
 den = [1 C+B*Kd B*Kp];
 
-step(num, den);
+system = tf(num, den)
+
+[Y, T] = step(num, den);
+
+% error terms
+E = 1-Y;
+E_dot = diff(E);
+E_dot = [0; E_dot];
+
+% calculating requested voltage using error
+V = Kp*E + Kd*E_dot;
+
+% scale_factor = max(V)/max(Y);
+
+% V_scaled = V/scale_factor + 1;
+V_scaled = V + 1
+
+figure();
+hold on
+title('System response and voltage requested');
+yyaxis right
+plot(Y)
+ylabel('system response');
+yyaxis left
+plot(V)
+
+% titlestr1 = sprintf('Requested voltage (scaled) by factor of %.3g (3 s.f)', scale_factor);
+titlestr1 = 'Requested voltage';
+ylabel(titlestr1);
