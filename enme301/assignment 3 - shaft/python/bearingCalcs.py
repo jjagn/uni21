c5ace2bffb6937ed2f45c959afd4072a89726379
jjagn <72364545+jjagn@users.noreply.github.com>
2021-05-24 21:01:39 +1200

    iterating to find bearings, 6322M passes

--- enme301/assignment 3 - shaft/python/bearingCalcs.py
+++ enme301/assignment 3 - shaft/python/bearingCalcs.py
@@ -3,29 +3,36 @@ import math
 # required revolutions in 20 year life
 required_life = 506.88
 
-f0 = 13.6 # calculation factor from bearing specs
-C = 229 # basic dynamic load rating C
-C0 = 216 # basic static load rating C0
-e = 0.29 # load ratio factor
-
-
-X = 0.56 # factor for axial load
-Y = 1.5 # factor for radial load
-
 c = 0.2 # distance of pulley from bearing
 
+# loads
 Fa = 18 # axial force in kN
 Fr = math.sqrt(4100.45 ** 2 + (7411.24 + 1543.32 * c) ** 2) / 1000 # radial force in kN
 
+# parameters from bearing datasheet
+f0 = 13.9 # calculation factor from bearing specs
+C = 276 # basic dynamic load rating C
+C0 = 285 # basic static load rating C0
+
+# calculation factor for finding e, X, and Y
 calc_factor = f0 * Fa / C0
 
+# from calc factor and tables
+e = 0.26 # load ratio factor
+X = 0.56 # factor for axial load
+Y = 1.6 # factor for radial load
+
 print(calc_factor)
+# print(Fa/Fr)
+# print(e)
 
-# print(Fr)
+if Fa/Fr <= e:
+    P = Fr
+else:
+    P = X*Fr + Y*Fa # equivalent dynamic load
 
-P = X*Fr + Y*Fa # equivalent dynamic load
-
-print("C must be > {:.2f}".format(8*P))
+# print("P = {:.2f}".format(P))
+# print("C must be > {:.2f}".format(8*P))
 
 rated_life = (C/P) ** 3
 
