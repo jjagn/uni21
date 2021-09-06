2d313e84cc903b81d4aedfb18295cbad82409e64
jjagn <72364545+jjagn@users.noreply.github.com>
2021-09-06 13:07:41 +1200

    starting on 302 assignment

--- enel372/solarCarCalcs.m
+++ enel372/solarCarCalcs.m
@@ -2,17 +2,22 @@ clear
 close all
 clc
 
-Vs = 17.5; % may be 14.5
-Vo = 2;
-Is = 0.26;
-Io = 2;
-% fs = 10 * 10^3;
-fs = linspace(10*10^3, 100*10^3, 500);
+Vs = 15; % may be 14.5
+Vo = 7;
+Is = 0.6;
+Io = 2.5;
+fs = 50 * 10^3;
+% fs = linspace(10*10^3, 100*10^3, 500);
 % Ipp = ;
 % Vpp = ;
 % D = linspace(0.36, 0.83, 100);
 D = .5;
 
-L = (Vs .* D .* (1 - D)) ./ (2 .* fs .* Io);
-    
-plot(fs, L)
+Lmin = Vs / (8 * fs * Io)
+dV = Vo/10;
+C = Io/(4*fs*dV) * 1e03
+
+Lmin * 1e03
+
+
+Ipp = Vs * (1-D) / (fs - L)
---
+++ enme302/assn1/plotDeflectedShapeModified.m
@@ -0,0 +1,33 @@
+function plotDeflectedShapeModified(node1XG, node1YG, d_e, magFactor, n, L, alpha)
+% plots deflected shape of beam using shape functions
+
+node2XG = node1XG + L * cosd(alpha);
+node2YG = node1YG + L * sind(alpha);
+
+x_e = linspace(0, L, n);
+phi_1 = 1 - x_e ./ L;
+phi_2 = x_e ./ L;
+
+N1 = 1 - 3 .* x_e .^ 2 ./ L^2 + 2 .* x_e .^ 3 ./L .^3;
+N2 = x_e .^ 3 ./ L .^ 2 - 2 .* x_e .^ 2 ./ L + x_e;
+N3 = 3 .* x_e .^ 2 ./ L .^ 2 - 2 .* x_e .^3 ./ L .^3;
+N4 = x_e .^ 3 ./ L .^ 2 - x_e .^ 2 ./ L;
+
+u = phi_1 .* d_e(1) + phi_2 .* d_e(4);
+v = N1 * d_e(2) +  N2 * d_e(3) +  N3  * d_e(5) + N4 * d_e(6);
+
+deflections_XG = u .* cosd(alpha) - v .* sind(alpha);
+deflections_YG = u .* sind(alpha) + v .* cosd(alpha);
+
+undeflected_XG = linspace(node1XG, node2XG, n);
+undeflected_YG = linspace(node1YG, node2YG, n);
+
+deflected_XG = undeflected_XG + magFactor .* deflections_XG;
+deflected_YG = undeflected_YG + magFactor .* deflections_YG;
+
+plot(undeflected_XG, undeflected_YG, 'b-')
+hold on
+plot(deflected_XG, deflected_YG, 'r-')
+legend('original shape', 'deflected shape')
+end
+
