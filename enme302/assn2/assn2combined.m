b85ffa8b4d675052d75ca0d5822bda754214cced
jjagn <72364545+jjagn@users.noreply.github.com>
2021-10-17 21:08:43 +1300

    combining first question

---
+++ enme302/assn2/assn2combined.m
@@ -0,0 +1,83 @@
+clc; close all; clear
+
+% VARIABLES
+L = 2;
+H = 3;
+
+g = 0;
+s = 0;
+c = 6;
+n = 10; % spatial grid points
+grid_size = 1; % physical size of grid
+t_n = 100; % number of time points
+t_f = 1; % end time of sim
+
+delta = grid_size/n;
+dt = t_f/t_n;
+
+lambda = c*dt/delta
+lambda2 = lambda*lambda
+
+% ANALYTICAL SOLUTION
+a_lim = 5;      % number of fourier series terms
+b_lim = 5;      % "" ""
+
+u_a = @(x, y, t) 0;
+
+for b = 1:1:b_lim
+    for a = 1:1:a_lim
+        u_a = @(x, y, t) u_a(x, y, t) + (576/pi^6) .* (((1 + (-1)^(a+1)) * (1+(-1)^(b+1))) / (a^3*b^3)).*sin(a.*pi./2.*x).*sin(b.*pi./3.*y).*cos(pi.*sqrt(9.*a.^2+4.*b.^2).*t);
+    end
+end
+
+x = linspace(0,L,L/delta+1);
+y = linspace(0,H,H/delta+1);
+t = linspace(0,t_f,t_n);
+
+[X,Y] = meshgrid(x, y);
+
+% loop to set initial conditions
+for i = 1:n*L
+    for j = 1:n*H
+        u_initial(i,j) = x(i).*y(j).*(L-x(i)).*(H-y(j));
+    end
+end
+
+for i = 1:t_n
+    results_analytical(:,:,i) = u_a(X, Y, t(i));
+end
+
+figure()
+surf(u_initial) % display initial conditions for verification
+
+% loop to find solutions, use if statements for boundary conditions
+u_p = u_initial;
+u_c = u_initial;
+for k = 1:t_n
+    for i = 1:n*L     
+        for j = 1:n*H
+            if i == 1 || j == 1 || i == n*L || j == n*H %% enforcing boundary conditions
+                u_n(i,j) = 0;
+            else
+                u_n(i,j) = 2*(1-2*lambda2)*u_c(i,j) + lambda2*(u_c(i+1,j) + u_c(i-1,j) + u_c(i,j+1) + u_c(i,j-1)) - u_p(i,j);
+            end         
+        end
+    end
+    u_p = u_c;
+    u_c = u_n;
+    results_numerical(:,:,k) = u_n;
+end
+
+figure()
+
+for time = 1:t_n
+    subplot(1,2,1)
+    surf(results_numerical(:,:,time));
+    axis([0 n*H 0 n*L -2.5 2.5]);
+    title('numerical solution')
+    subplot(1,2,2)
+    surf(results_analytical(:,:,time));
+    axis([0 n*H 0 n*L -2.5 2.5]);
+    title('analytical solution')
+    pause(1/60)
+end
