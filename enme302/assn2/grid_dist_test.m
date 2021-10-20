6527e1f7cd5469a5ac2a688e6fa3c4e8dfb2a125
jjagn <72364545+jjagn@users.noreply.github.com>
2021-10-20 22:33:21 +1300

    grid test code

---
+++ enme302/assn2/grid_dist_test.m
@@ -0,0 +1,32 @@
+clear; close all; clc
+alpha = 0.3;
+L = 2;
+H = 3;
+N = 100;
+
+x_points = generate_grid(L, N, alpha);
+y_points = generate_grid(H,N,alpha);
+
+error = zeros(1, N+1);
+for i = 1:N-1
+    error(i) = x_points(i+1) - x_points(i);
+end
+
+[X,Y] = meshgrid(x_points, y_points);
+
+surf(X,Y,X*0)
+
+function grid_points = generate_grid(L,N,alpha)
+x_k = @(alpha,K,L,n) L*alpha*sum((1+alpha).^(K-1))/(2*((1+alpha)^n-1));
+
+n = N/2;
+X = zeros(1,n);
+for i  = 1:n-1
+    K = 1:i;
+    X(i+1) = x_k(alpha, K, L, n);
+end
+
+grid_points = zeros(1,N);
+grid_points(1, 1:n) = X;
+grid_points(1, n+1:N) = L-fliplr(X);
+end
